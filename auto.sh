b574eeb163f054e2d1c128a96f73e5b5872400e2
jimmywang0417 <jimmywang0417@gmail.com>
2025-08-03 16:17:28 +0800

    Site updated: 2025-08-03 16:17:28

---
+++ auto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 获取当前时间
+TIME=$(date +"%Y-%m-%d %H:%M:%S")
+
+# 添加所有修改
+git add -A
+
+# 提交
+git commit -m "Site updated: $TIME"
+
+# 推送（如果你希望自动推送）
+git push
+
+# 推送 github actions
+hexo clean && hexo g && hexo d
